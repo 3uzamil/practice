038cdf6db7e28f44317ebf595a381f3ec181cbfc
Muzamil Hussain <3uzamilhussain@gmail.com>
2025-11-24 18:28:30 +0500

    feat: update on (21-Nov-2025)

---
+++ practice3.js
@@ -0,0 +1,20 @@
+// const user = {
+//     name: "Muzamil",
+//     age: 17
+// };
+
+// console.log(user.name);
+
+const car = {
+    make: "Honda",
+    model: "Civic",
+    start(){
+        console.log("Engine Start");
+    },
+    stop(){
+        console.log("Engine Stop")
+    },
+
+};
+
+

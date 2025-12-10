62384389dd0f3dc47c228ebeda40fbf116eb70c0
Muzamil Hussain <3uzamilhussain@gmail.com>
2025-12-10 19:54:36 +0500

    update on (10-Dec-2025)

---
+++ 10-12-2025/app.js
@@ -0,0 +1,13 @@
+// class Car {
+//     constructor(brand){
+//         this.brand = brand;
+//     }
+//     start(){
+//         return this.brand + ' is starting...';
+//     }
+// };
+// let toyota = new Car('Toyota');
+// console.log(toyota.start());
+
+// import { add } from "./math.js";
+// console.log(add(4,12));
